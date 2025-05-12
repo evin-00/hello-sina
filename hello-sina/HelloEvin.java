12ea933247fc6742e59f151662f7fb8879e5b732
evin-00 <evin.sina@capellinisauro.edu.it>
2025-05-12 07:15:00 +0000

    stampa nome

--- hello-sina/HelloEvin.java
+++ hello-sina/HelloEvin.java
@@ -1,6 +1,8 @@
 import java.io.*;
 public class HelloEvin {
     public static void main(String[] args) {
-        System.out.println("hello, world!");
+        System.out.println("hello, Evin");
+        System.out.println("hello, Evin");
+        System.out.println("hello, Evin");
     }
 }
